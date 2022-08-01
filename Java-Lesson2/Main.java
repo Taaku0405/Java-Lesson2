ae8bd4fcfe698daceece90f3a09fcdc6bda2fb00
Taaku0405 <rakosutaa@gmail.com>
2022-08-01 16:53:30 +0900

    Java学習

---
+++ Java-Lesson2/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    // 真偽値と条件式の学習
+    System.out.println(true);
+    System.out.println(false);
+    System.out.println(12 / 4 == 3);
+    System.out.println(12 / 4 != 3);
+    boolean bool = 3 * 9 == 27;
+    System.out.println(bool);
+
+  }
+}
