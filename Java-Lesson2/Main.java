471f0daea5bb0c83a1ff3099e4257e3e92e81490
Taaku0405 <rakosutaa@gmail.com>
2022-08-02 18:08:26 +0900

    学習

--- Java-Lesson2/Main.java
+++ Java-Lesson2/Main.java
@@ -11,5 +11,34 @@ class Main {
     // 比較演算子の学習
     System.out.println(4 + 2 < 6);
     System.out.println(4 + 2 <= 6);
+
+    // 論理演算子の学習 『かつ』= && 『または』 = || 『〜でない』 = !(◯ >= △)
+    System.out.println(true || false);
+    System.out.println(false && true);
+    // 8 < 5 かつ 3 >= 2 の結果を出力
+    System.out.println(8 < 5 && 3 >= 2);
+    // 8 < 5 または 3 >= 2 の結果を出力
+    System.out.println(8 < 5 || 3 >= 2);
+    // 8 < 5 でないの結果を出力
+    System.out.println(!(8 < 5));
+
+    // 条件分岐ifを使った学習
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+
+    int x = 5;
+    if (x > 2) {
+      System.out.println("xは２より大きい");
+    }
+
+    if (x > 8) {
+      System.out.println("xは8より大きい");
+    }
+
   }
 }
