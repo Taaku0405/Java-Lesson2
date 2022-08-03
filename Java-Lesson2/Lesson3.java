efb0b67152e619aa06977890357cd004240f2ba9
Taaku0405 <rakosutaa@gmail.com>
2022-08-03 16:40:19 +0900

    学習

---
+++ Java-Lesson2/Lesson3.java
@@ -0,0 +1,16 @@
+public class Lesson3 {
+  public static void main(String[] args) {
+    // 配列の学習 => namesに複数の値を入れる
+    String[] names = { "マイケル", "ボブ", "キャサリン" };
+    // マイケルを出力
+    System.out.println(names[0]);
+    // キャサリンを出力
+    System.out.println(names[2]);
+
+    // 配列要素の上書きの学習
+    String[] languages = { "Ruby", "PHP", "Python" };
+    System.out.println(languages[1]);
+    languages[1] = "Java";
+    System.out.println(languages[1]);
+  }
+}
